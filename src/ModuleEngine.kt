ab8e3dcb113526b7b0dccb42fd262b789422b779
DanyZy <daniilzgurovec@gmail.com>
2020-03-11 23:00:59 +0600

    some cosmetic changes

--- src/ModuleEngine.kt
+++ src/ModuleEngine.kt
@@ -3,31 +3,36 @@ import java.io.File
 object ModuleEngine {
     @JvmStatic
     fun main(args: Array<String>) {
-        val modulePath = "C:\\Users\\Daniil\\IdeaProjects\\Construction_materials\\regularTest\\out\\production\\regularTest"
+        // Test path, change later
+        val modulePath = "C:\\Users\\Daniil\\IdeaProjects\\Construction_materials\\regularTest\\out\\production\\regularTest\\"
         /**
-         * Создаем загрузчик модулей.
+         * Create a module loader
          */
         val loader = ModuleLoader(modulePath, ClassLoader.getSystemClassLoader())
         /**
-         * Получаем список доступных модулей.
+         * Get a list of available modules
          */
         val dir = File(modulePath)
         val modules = dir.list()
         /**
-         * Загружаем и исполняем каждый модуль.
+         * Load and execute each module
          */
-        for (module in modules) {
-            try {
-                val moduleName = module.split(".class".toRegex()).toTypedArray()[0]
-                val clazz = loader.loadClass(moduleName)
-                val execute: IPlugin = clazz.newInstance() as IPlugin
-                execute.load()
-            } catch (e: ClassNotFoundException) {
-                e.printStackTrace()
-            } catch (e: InstantiationException) {
-                e.printStackTrace()
-            } catch (e: IllegalAccessException) {
-                e.printStackTrace()
+        if (modules != null) {
+            for (module in modules) {
+                try {
+                    val moduleName = module.split(".class".toRegex()).toTypedArray()[0]
+                    val clazz = loader.loadClass(moduleName)
+                    val execute: IPlugin = clazz.newInstance() as IPlugin
+                    execute.load()
+                } catch (ex: Exception) {
+                    when (ex) {
+                        is ClassNotFoundException,
+                        is InstantiationException,
+                        is IllegalAccessException -> {
+                            ex.printStackTrace()
+                        }
+                    }
+                }
             }
         }
     }
--- src/ModuleLoader.kt
+++ src/ModuleLoader.kt
@@ -1,44 +1,59 @@
 import java.io.*
 
 class ModuleLoader(private val pathtobin: String, parent: ClassLoader?): ClassLoader(parent) {
-
-    // TODO: Добавить метод загрузки динамических библиотек
-
+    
     @Throws(ClassNotFoundException::class)
     public override fun findClass(className: String): Class<*> {
         return try {
-            val b = fetchClassFromFS("$pathtobin$className.class")
-            defineClass(className, b, 0, b.size)
-        } catch (ex: FileNotFoundException) {
-            super.findClass(className)
-        } catch (ex: IOException) {
-            super.findClass(className)
+            val bytes = fetchClassBytes("$pathtobin$className.class")
+            defineClass(className, bytes, 0, bytes.size)
+        } catch (ex: Exception) {
+            when (ex) {
+                is FileNotFoundException,
+                is IOException -> {
+                    super.findClass(className)
+                }
+                else -> throw ex
+            }
         }
     }
 
     @Throws(FileNotFoundException::class, IOException::class)
-    private fun fetchClassFromFS(path: String): ByteArray {
-        val `is`: InputStream = FileInputStream(File(path))
-        // Get the size of the file
+    private fun fetchClassBytes(path: String): ByteArray {
+        val inputStream: InputStream = FileInputStream(File(path))
+        /**
+        * Get the size of the file
+        */
         val length = File(path).length()
-        if (length > Int.MAX_VALUE) { // File is too large
+        if (length > Int.MAX_VALUE)
+        {
+            /**
+             * File is too large
+             */
         }
-        // Create the byte array to hold the data
+        /**
+         * Create the byte array to hold the data
+         */
         val bytes = ByteArray(length.toInt())
-        // Read in the bytes
+        /**
+         * Read in the bytes
+         */
         var offset = 0
         var numRead = 0
         while (offset < bytes.size
-            && `is`.read(bytes, offset, bytes.size - offset).also { numRead = it } >= 0
-        ) {
+            && inputStream.read(bytes, offset, bytes.size - offset).also { numRead = it } >= 0) {
             offset += numRead
         }
-        // Ensure all the bytes have been read in
+        /**
+         * Ensure all the bytes have been read in
+         */
         if (offset < bytes.size) {
             throw IOException("Could not completely read file $path")
         }
-        // Close the input stream and return bytes
-        `is`.close()
+        /**
+         * Close the input stream and return bytes
+         */
+        inputStream.close()
         return bytes
     }
 
